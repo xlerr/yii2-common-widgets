e0e58fe98986550d10a0162ebbd43b4231e5276b
xlerr <xlerr@qq.com>
2019-08-16 18:13:35 +0800

    优化金额输入框实现

--- src/widgets/MoneyInput.php
+++ src/widgets/MoneyInput.php
@@ -2,44 +2,66 @@
 
 namespace xlerr\common\widgets;
 
+use yii\helpers\Json;
+use yii\web\JsExpression;
 use yii\widgets\MaskedInput;
+use yii\widgets\MaskedInputAsset;
 
 class MoneyInput extends MaskedInput
 {
     public $clientOptions = [
-        'alias'               => 'numeric',
-        'digits'              => 2,
-        'groupSeparator'      => ',',
-        'radixPoint'          => '.',
-        'autoGroup'           => true,
-        'autoUnmask'          => true,
-        'rightAlign'          => false,
-        'enforceDigitsOnBlur' => true,
-        'removeMaskOnSubmit'  => true,
+        'alias' => 'money',
     ];
 
     public function init()
     {
         parent::init();
 
-        $this->clientOptions += [
-            'onUnMask' => 'function(maskedValue) { return (Number(maskedValue.replace(/,/g, \'\')) * 100).toFixed(0); }',
+        $this->aliases['money'] = [
+            'alias'               => 'numeric',
+            'digits'              => 2,
+            'groupSeparator'      => ',',
+            'radixPoint'          => '.',
+            'autoGroup'           => true,
+            'autoUnmask'          => true,
+            'rightAlign'          => false,
+            'enforceDigitsOnBlur' => true,
+            'removeMaskOnSubmit'  => true,
+            'onBeforeMask'        => new JsExpression('function (v) { v = Number(v); return (isNaN(v) ? 0 : v / 100).toFixed(2) }'),
+            'onUnMask'            => new JsExpression('function(v) { return (Number(v.replace(/,/g, \'\')) * 100).toFixed(0) }'),
         ];
     }
 
     public function registerClientScript()
     {
-        parent::registerClientScript();
+        $this->initClientOptions();
+        if (!empty($this->mask)) {
+            $this->clientOptions['mask'] = $this->mask;
+        }
 
-        $id = $this->options['id'];
+        $view = $this->getView();
+        MaskedInputAsset::register($view);
 
-        $value = $this->hasModel() ? $this->model->{$this->attribute} : $this->value;
-        $value = round($value / 100, 2);
+        $this->hashPluginOptions($view);
 
-        $js = <<<EOF
-jQuery("#{$id}").val({$value});
-EOF;
+        if (is_array($this->definitions) && !empty($this->definitions)) {
+            $view->registerJs(vsprintf('%s.extendDefinitions(%s);', [
+                ucfirst(self::PLUGIN_NAME),
+                Json::htmlEncode($this->definitions),
+            ]));
+        }
 
-        $this->getView()->registerJs($js);
+        if (is_array($this->aliases) && !empty($this->aliases)) {
+            $view->registerJs(vsprintf('%s.extendAliases(%s);', [
+                ucfirst(self::PLUGIN_NAME),
+                Json::htmlEncode($this->aliases),
+            ]));
+        }
+
+        $view->registerJs(vsprintf('jQuery("#%s").%s(%s);', [
+            $this->options['id'],
+            self::PLUGIN_NAME,
+            $this->_hashVar,
+        ]));
     }
 }
