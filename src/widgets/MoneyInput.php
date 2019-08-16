fd0cd25f6843e7ec105c7965166ba57fb1823749
xlerr <xlerr@qq.com>
2019-08-16 16:27:25 +0800

    优化金额输入框实现

--- src/widgets/MoneyInput.php
+++ src/widgets/MoneyInput.php
@@ -16,6 +16,30 @@ class MoneyInput extends MaskedInput
         'rightAlign'          => false,
         'enforceDigitsOnBlur' => true,
         'removeMaskOnSubmit'  => true,
-        'onUnMask'            => 'function(maskedValue) { return (Number(maskedValue.replace(/,/g, \'\')) * 100).toFixed(0); }',
     ];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->clientOptions += [
+            'onUnMask' => 'function(maskedValue) { return (Number(maskedValue.replace(/,/g, \'\')) * 100).toFixed(0); }',
+        ];
+    }
+
+    public function registerClientScript()
+    {
+        parent::registerClientScript();
+
+        $id = $this->options['id'];
+
+        $value = $this->hasModel() ? $this->model->{$this->attribute} : $this->value;
+        $value = round($value / 100, 2);
+
+        $js = <<<EOF
+jQuery("#{$id}").val({$value});
+EOF;
+
+        $this->getView()->registerJs($js);
+    }
 }
